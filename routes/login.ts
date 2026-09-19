baf8141f9ca185ae9d7c85d57ac70224a3440658
Akash Pradhan <akashpradhan439@gmail.com>
2026-09-18 22:27:11 -0700

    login schema fix

--- routes/login.ts
+++ routes/login.ts
@@ -33,7 +33,7 @@ function generateTokens(userId: string, countryCode: string, phoneNumber: string
 
 const LoginSchema = z.object({
   country_code: z.string().min(1),
-  phone_number: z.string().min(1),
+  phone_number: z.number(),
   password: z.string().min(1),
   force_login: z.boolean().optional(),
   force_login_token: z.string().optional(),
